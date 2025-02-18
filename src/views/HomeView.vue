9f3b50646730e23a5a3be314f494efd7face398b
coldsoul <inhailmedia@gmail.com>
2025-02-17 20:40:18 -0800

    base checkin

---
+++ src/views/HomeView.vue
@@ -0,0 +1,196 @@
+<template>
+
+  <h1>Speech Recognition Demo</h1>
+  <p>using webRTC & webkit speech recognition.</p>
+
+  <div class="input-container" :class="{micActive: isMicActive, isTalking: isTalking }">
+    <input v-model="transcript" type="text" placeholder="Say words and they appear here!" />
+    <button @click="toggleRecording">
+      {{ isRecording ? 'Stop Recording' : 'Start Recording' }}
+    </button>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const transcript = ref('');
+const isRecording = ref(false);
+let recognition = null;
+let isMicActive = false;
+let isTalking = false;
+let silenceTimeout = null;
+let silenceAmount = 3000; // 3 seconds
+
+if ('webkitSpeechRecognition' in window) {
+  // eslint-disable-next-line no-undef
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.lang = 'en-US';
+
+  recognition.onresult = (event) => {
+    isTalking = true;
+
+    let fullTranscript = '';
+    for (let i = 0; i < event.results.length; i++) {
+      fullTranscript += event.results[i][0].transcript + ' ';
+    }
+    transcript.value = fullTranscript.trim();
+    resetSilenceTimer();
+  }
+
+  recognition.onspeechend = () => {
+    startSilenceTimer();
+    // stopRecording();
+  }
+}
+
+const toggleRecording = () => {
+  if (isRecording.value) {
+    stopRecording();
+  } else {
+    startRecording();
+  }
+}
+
+const startRecording = () => {
+  if (recognition) {
+    transcript.value = '';
+    isRecording.value = true;
+    isMicActive = true;
+    recognition.start();
+    resetSilenceTimer();
+
+  }
+}
+
+const stopRecording = () => {
+  if (recognition) {
+    isRecording.value = false;
+    recognition.stop();
+    isMicActive = false;
+    clearTimeout(silenceTimeout);
+  }
+}
+
+const startSilenceTimer = () => {
+  clearTimeout(silenceTimeout);
+  silenceTimeout = setTimeout(() => {
+    stopRecording();
+    isTalking = false;
+  }, silenceAmount); 
+};
+
+const resetSilenceTimer = () => {
+  clearTimeout(silenceTimeout);
+  startSilenceTimer();
+};
+</script>
+
+<style lang="scss" scoped>
+
+// variables
+$borderWidth: 3px;
+$black: #222222;
+$green: #21FA90;
+$red: #F02D3A;
+$purple: #9191E9;
+
+
+h1 {
+  text-align: center;
+  padding: 40px 0 0 0;
+  color: rgb(203, 203, 203);
+}
+p {
+  text-align: center;
+  padding:  0 0 20px 0;
+}
+
+.input-container {
+  display: block;
+  position: relative;
+  width: 800px;
+  margin: auto;
+
+}
+
+.micActive {
+  &:after {
+    content: '';
+    position: absolute;
+    top: calc(-1 * $borderWidth);
+    left: calc(-1 * $borderWidth);
+    height: calc(100% + $borderWidth * 2);
+    width: calc(100% + $borderWidth * 2);
+    background: linear-gradient(
+      60deg,
+      #c633f7,
+      #f37055,
+      #ef4e7b,
+      #a166ab,
+      #5073b8,
+      #1098ad,
+      #07b39b,
+      #6fba82
+    );
+    border-radius: 10px;
+    z-index: -1;
+    background-size: 400% 400%;
+    filter:blur(10px);
+  }
+}
+
+.isTalking {
+  &:after {
+    animation: animatedgradient 3s ease alternate infinite;
+  }
+}
+
+input {
+  position: relative;
+  display: inline-block;
+  padding: 20px;
+  width: calc(100% - 132.6px);
+  border-radius: 10px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+  
+}
+
+button {
+  padding: 20px;
+  background-color: #373737fc;
+  color: white;
+  border: none;
+  cursor: pointer;
+  border-radius: 10px;
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  transition: 0.5s all ease-in-out;
+
+  &:hover {
+    background-color: #08af21;
+    transition: 0.5s all ease-in-out;
+  }
+}
+
+@keyframes animatedgradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+</style>
